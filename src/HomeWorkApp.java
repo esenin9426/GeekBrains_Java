0ab4f6d1fd027b085349ee45bed34a25d5fb6aa9
Anton Korotkov <akorotko@MacBook-Pro-Anton.local>
2021-07-14 15:55:17 +0300

    HW1

---
+++ src/HomeWorkApp.java
@@ -0,0 +1,36 @@
+public class HomeWorkApp {
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+    static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+    static void checkSumSign(){
+        int a = 5;
+        int b = -10;
+        if (a + b >= 0) System.out.println("Сумма положительная");
+        else System.out.println("Сумма отрицательная");
+    }
+
+    static void printColor(){
+        int value = 101;
+        if (value <= 0) System.out.println("Красный");
+        else if (value > 0 && value <= 100) System.out.println("Желтый");
+        else System.out.println("Зеленый");
+    }
+
+    static void compareNumbers(){
+        int a = 5;
+        int b = 10;
+        if (a>= b) System.out.println("a >= b");
+        else System.out.println("a < b");
+    }
+}
