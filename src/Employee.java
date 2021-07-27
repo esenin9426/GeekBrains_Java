b65ec70f610ef686542a5a30fdda65ed1e5979b0
Anton Korotkov <akorotko@MacBook-Pro-Anton.local>
2021-07-27 14:09:31 +0300

    HW5

---
+++ src/Employee.java
@@ -0,0 +1,100 @@
+public class Employee {
+
+    private String surname;
+    private String name;
+    private String patronymic;
+    private String position;
+    private String email;
+    private String phone;
+    private float salary;
+    private int age;
+
+    public Employee(String surname, String name, String patronymic, String position, String email, String phone, float salary, int age) {
+        this.surname = surname;
+        this.name = name;
+        this.patronymic = patronymic;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "surname='" + surname + '\'' +
+                ", name='" + name + '\'' +
+                ", patronymic='" + patronymic + '\'' +
+                ", position='" + position + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
