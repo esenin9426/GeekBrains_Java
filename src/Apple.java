f24c37bae58f2b2a69b2db6f42c47430e248ee3f
akorotko <esenin9426@gmail.com>
2022-01-25 23:25:08 +0300

    3HW1

---
+++ src/Apple.java
@@ -0,0 +1,8 @@
+public class Apple implements Fruit{
+
+    private double weight = 1.5;
+
+    public double getWeight() {
+        return weight;
+    }
+}
---
+++ src/Orange.java
@@ -0,0 +1,8 @@
+public class Orange implements Fruit {
+
+    private double weight = 1.0;
+
+    public double getWeight() {
+        return weight;
+    }
+}
